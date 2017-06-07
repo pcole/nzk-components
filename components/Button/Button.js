84db7804b04ecb18a83dfcc880f77f9a5fa1d97a
pcole <philip@pcole.me>
2017-06-07 18:09:21 +0100

    Button component and initial stories

---
+++ components/Button/Button.js
@@ -0,0 +1,126 @@
+// @flow
+
+import React, { Component } from 'react'
+import Color from 'color'
+import cn from 'classnames'
+import styles from './styles'
+ 
+export type ButtonType = "button" | "reset" | "submit"
+export type ButtonSizeType = "x-small" | "small" | "regular" | "large" | "x-large"
+
+type ButtonPropsType = {
+	active: boolean,
+	disabled: boolean,
+	block: boolean,
+	shadow: boolean,
+	gradient: boolean,
+	round: boolean,
+	onClick: () => void,
+	bgColor: string,
+	bgColorTo: string,
+	type: ButtonType,
+	color: string,
+	size: ButtonSizeType,
+	width: string,
+	height: string,
+	borderRadius: string,
+	componentClass: string,
+	children: Object,
+}
+
+export default class Button extends Component<void, ButtonPropsType, void>	{
+
+	handleClick: Function
+
+	constructor(props: ButtonPropsType) {
+		super(props)
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick(event: Event) {
+		const { disabled, onClick } = this.props
+
+		if (disabled) {
+			event.preventDefault()
+			event.stopPropagation()
+			return
+		}
+
+		if (onClick) {
+			onClick(event)
+		}
+	}
+
+	render() {
+		const {
+			disabled,
+			children,
+			active,
+			block,
+			color,
+			round,
+			size,
+			width,
+			height,
+			bgColor,
+			bgColorTo,
+			borderRadius,
+			shadow,
+			gradient,
+			componentClass,
+			...props } = this.props
+			
+		if (disabled) {
+			props.tabIndex = -1
+			props.style = { pointerEvents: 'none', ...props.style }
+		}
+
+		const className = cn({
+			button: true,
+			disabled: disabled,
+			active: active,
+			block: block,
+			shadow: shadow,
+			round: round,
+			[size]: size,
+		})
+
+		const bgColorObj = Color(bgColor || '#55acf1')
+		const bgLuminosity = bgColorObj.luminosity() 
+		const colorObj = Color(color || (bgLuminosity >= 0.5 ? 'black' : 'white'))
+
+		props.style = {
+			'color': colorObj.string(),
+			'backgroundColor': bgColorObj.string(),
+			'borderRadius': round ? '50%' : borderRadius || '20px',
+			'boxShadow': shadow
+				? `0px 4px 0px ${bgColorObj.darken(.25).string()}` 
+				: 'none',
+			'backgroundImage': gradient || bgColorTo
+				? `linear-gradient(to right, ${bgColor}, ${bgColorTo || bgColorObj.lighten(.20).string()})`
+				: 'none',
+			...props.style
+		}
+
+		if (width) {
+			props.style = {
+				'width': width,
+				...props.style
+			}
+		}
+
+		 if (height) {
+			props.style = {
+				'height': height,
+				...props.style
+			}
+		}
+
+		switch(componentClass){
+			case 'span': return <span {...props} className={className}>{ children }<style jsx>{styles}</style></span>
+			case 'button': return <button {...props} className={className}>{ children }<style jsx>{styles}</style></button>
+			case 'a': return <a {...props} className={className}>{ children }<style jsx>{styles}</style></a>
+			default: return <div {...props} className={className}>{ children }<style jsx>{styles}</style></div>
+		}
+	}
+}
---
+++ components/Button/Button.stories.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs, text, color, select, boolean } from '@storybook/addon-knobs'
+import Button from './Button'
+import Icon from '../Icon/Icon'
+
+const buttonSizes = {
+	'x-small': 'x-small',
+	'small': 'small',
+	'regular': 'regular',
+	'large': 'large',
+	'x-large': 'x-large',
+};
+
+storiesOf('Button', module)
+	.addDecorator(withKnobs)
+	.add('Plain button', () => (
+		<Button
+			bgColor={color('bgColor', '#55acf1')}
+			color={color('color', '#fff')}
+			size={select('size', buttonSizes, 'regular')}
+		>{text('Label', 'A simple button')}</Button>
+	))
+	.add('With shadow and gradient', () => (
+		<Button
+			bgColor={color('bgColor', '#55acf1')}
+			color={color('color', '#fff')}
+			size={select('size', buttonSizes, 'regular')}
+			shadow={boolean('shadow', true)}
+			gradient={boolean('gradient', true)}
+		>{text('Label', 'With shadow & gradient')}</Button>
+	))
+	.add('Custom gradient', () => (
+		<Button
+			bgColor={color('bgColor', '#81247A')}
+			bgColorTo={color('bgColorTo', '#303993')}
+			color={color('color', '#fff')}
+			size={select('size', buttonSizes, 'regular')}
+			shadow={boolean('shadow', true)}
+		>{text('Label', 'Custom gradient')}</Button>
+	))
+	.add('Round with icon', () => (
+		<Button
+			round={true}
+			bgColor={color('bgColor', '#81247A')}
+			color={color('color', '#fff')}
+			size={select('size', buttonSizes, 'regular')}
+			shadow={boolean('shadow', true)}
+		><Icon name={text('Icon name', 'pencil')} /></Button>
+	))
+	.add('Dispay block', () => (
+		<Button
+			block={boolean('block', true)}
+		>I display block</Button>
+	))
+	.add('Custom dimensions', () => (
+		<Button
+			width={text('Width', '300px')}
+			height={text('height', '300px')}
+		>I can be any size and the text is centered and multiline</Button>
+	))
