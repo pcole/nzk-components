b2866250e05a681a1802930cb33ed97e9999900d
pcole <philip@pcole.me>
2017-06-22 16:22:51 +0100

    Build

---
+++ dist/Uploader/Uploader.js
@@ -0,0 +1,174 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _style = require('styled-jsx/style');
+
+var _style2 = _interopRequireDefault(_style);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _Uploader = require('./Uploader.styles');
+
+var _Uploader2 = _interopRequireDefault(_Uploader);
+
+var _reactDropzone = require('react-dropzone');
+
+var _reactDropzone2 = _interopRequireDefault(_reactDropzone);
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Uploader = function (_React$Component) {
+  _inherits(Uploader, _React$Component);
+
+  function Uploader(props) {
+    _classCallCheck(this, Uploader);
+
+    var _this = _possibleConstructorReturn(this, (Uploader.__proto__ || Object.getPrototypeOf(Uploader)).call(this, props));
+
+    _this.state = {
+      progress: 0,
+      images: []
+    };
+    _this.onDrop = _this.onDrop.bind(_this);
+    return _this;
+  }
+
+  _createClass(Uploader, [{
+    key: 'uploadOver',
+    value: function uploadOver() {
+      var _this2 = this;
+
+      setTimeout(function () {
+        _this2.setState({ progress: 0 });
+      }, 2000);
+    }
+  }, {
+    key: 'onDrop',
+    value: function onDrop(file) {
+      var _this3 = this;
+
+      this.setState(function () {
+        return {
+          progress: 0
+        };
+      });
+      var data = new window.FormData();
+      data.append('image', file[0]);
+
+      var config = {
+        onUploadProgress: function onUploadProgress(progressEvent) {
+          var percentCompleted = Math.round(progressEvent.loaded * 100 / progressEvent.total);
+          _this3.setState({ progress: percentCompleted });
+          if (percentCompleted === 100) {
+            _this3.uploadOver();
+          }
+        }
+      };
+      var _ = this;
+      _axios2.default.post(this.props.api, data, config).then(function (res) {
+        var images = _this3.state.images.slice();
+        images.push(res.data.url);
+        _.setState(function () {
+          return {
+            images: images
+          };
+        });
+        console.log(res.data);
+      }).catch(function (err) {
+        window.alert('Erroooor');
+        console.log(err);
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        {
+          'data-jsx-ext': _Uploader2.default.__scopedHash
+        },
+        _react2.default.createElement(
+          'div',
+          {
+            className: 'drop-zone',
+            style: {
+              position: 'relative',
+              height: '200px',
+              width: '200px'
+            },
+            'data-jsx-ext': _Uploader2.default.__scopedHash
+          },
+          _react2.default.createElement(
+            'div',
+            {
+              className: 'progress',
+              style: {
+                width: this.state.progress + '%',
+                background: 'rgba(107, 188, 102, 0.7)'
+              },
+              'data-jsx-ext': _Uploader2.default.__scopedHash
+            },
+            ' '
+          ),
+          this.props.api ? _react2.default.createElement(
+            _reactDropzone2.default,
+            { accept: 'image/jpeg, image/png', onDrop: this.onDrop },
+            this.state.progress > 0 ? _react2.default.createElement(
+              'div',
+              { className: 'progress-label', 'data-jsx-ext': _Uploader2.default.__scopedHash
+              },
+              this.state.progress,
+              '%'
+            ) : null
+          ) : _react2.default.createElement(
+            'p',
+            {
+              'data-jsx-ext': _Uploader2.default.__scopedHash
+            },
+            ' Missing API to Component '
+          )
+        ),
+        _react2.default.createElement(
+          'ul',
+          {
+            'data-jsx-ext': _Uploader2.default.__scopedHash
+          },
+          this.state.images.map(function (img, i) {
+            return _react2.default.createElement(
+              'li',
+              { key: i, 'data-jsx-ext': _Uploader2.default.__scopedHash
+              },
+              _react2.default.createElement('img', { src: img, style: { width: '80%' }, 'data-jsx-ext': _Uploader2.default.__scopedHash
+              })
+            );
+          })
+        ),
+        _react2.default.createElement(_style2.default, {
+          styleId: _Uploader2.default.__scopedHash,
+          css: _Uploader2.default.__scoped
+        })
+      );
+    }
+  }]);
+
+  return Uploader;
+}(_react2.default.Component);
+
+exports.default = Uploader;
---
+++ dist/Uploader/Uploader.styles.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+   value: true
+});
+
+var __styledJsxDefaultExport = new String(".progress{position:absolute;top:2px;left:2px;height:100%;z-index:-1;border-radius:3px}.progress-label{-webkit-text-align:center;text-align:center;font-size:40px;font-weight:bold;line-height:200px;color:white}");
+
+__styledJsxDefaultExport.__hash = "13304619947";
+__styledJsxDefaultExport.__scoped = ".progress[data-jsx-ext~=\"23304619947\"]{position:absolute;top:2px;left:2px;height:100%;z-index:-1;border-radius:3px}.progress-label[data-jsx-ext~=\"23304619947\"]{-webkit-text-align:center;text-align:center;font-size:40px;font-weight:bold;line-height:200px;color:white}";
+__styledJsxDefaultExport.__scopedHash = "23304619947";
+exports.default = __styledJsxDefaultExport;
